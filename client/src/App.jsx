6978994305e63cbbfd672a9d1aca132c4b81498d
kevinsultana <kevinpdg.ks@gmail.com>
2025-06-26 14:55:57 +0700

    feat: added get function and json server running

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BaseApi } from "./Api/BaseApi";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState([]);
+  const [todoData, setTodoData] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  const getUser = async () => {
+    try {
+      const response = await BaseApi.get("/user");
+      setUser(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getTodoById = async (id) => {
+    try {
+      const response = await BaseApi.get(`/todos/?userId=${id}`);
+      setTodoData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Todo Apps</h1>
+      {selectedUser === null && (
+        <div>
+          <h3>User List</h3>
+          {user.map((item) => (
+            <div
+              onClick={() => {
+                getTodoById(item.id), setSelectedUser(item);
+              }}
+              key={item.id}
+            >
+              <h5>{item.name}</h5>
+            </div>
+          ))}
+        </div>
+      )}
+      {selectedUser !== null && (
+        <div>
+          <h3>Todo List</h3>
+          {todoData.map((item) => (
+            <div key={item.id}>
+              <h5>{item.title}</h5>
+              <p>{item.description}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
