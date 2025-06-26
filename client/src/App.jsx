5341299b95e362a157f7da0985969eed80909e11
kevinsultana <kevinpdg.ks@gmail.com>
2025-06-26 16:18:34 +0700

    testing post json server api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Router, Routes } from "react-router";
-import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,12 +24,41 @@ export default function Home() {
     }
   };
 
+  const createNewUser = async () => {
+    try {
+      const response = await BaseApi.post("/user", {
+        name: "admin",
+        password: "admin",
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const createNewTodoById = async () => {
+    try {
+      const response = await BaseApi.post("/todos", {
+        title: "Learn React",
+        description: "Learn React",
+        completed: false,
+        userId: "7f3a",
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     getUser();
   }, []);
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      {/* <button onClick={createNewUser}>Create New User</button> */}
+      <button onClick={() => setSelectedUser(null)}>log out</button>
+      <button onClick={() => createNewTodoById()}>createNewTodoById</button>
       <h1>Todo Apps</h1>
       {selectedUser === null && (
         <div>
